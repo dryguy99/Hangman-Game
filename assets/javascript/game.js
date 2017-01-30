940f308543f4c5854c2fde5b11e331fc714b7eba
Timothy Dry <timothy.dry@verizon.net>
2017-01-30 14:02:02 -0500

    random movie pick works added tracking guesses- not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,5 +2,20 @@
 var guessCounter = 12;
 var winCounter = 0;
 var lossCounter = 0;
-var movieArray = ["Spotlight", "Birdman", "12 Years a Slave", "Argo", "The Artist", "The Kings Speach", "The Hurt Locker", "Slumdog Millionaire", "No Country for Old Men", "The Departed", "Crash", "Million Dollar Baby", "The Return of the King", "Chicago", "A Beautiful Mind", "Gladiator", "American Beauty", "Titanic", "Shakespeare in Love", "The English Patient", "Braveheart", "Forest Gump", "Schindlers List", "Unforgiven", "Dances With Wolves", "Driving Miss Daisy", "The Last Emperor", "Platoon", "Out of Aferica", "Amadeus", "Terms Of Endearment", "Ganghi", "Chariots Of Fire", "Ordinary People", "Kramer vs Kramer", "The Deer Hunter", "Annie Hall", "Rocky", "One Flew Over The Cuckoos Nest", "The Godfather Part 2", "The Sting", "The Godfather", "The French Connection", "Patton","Midnight Cowboy", "Oliver"]
-var pressedButtonsArray = [" "]
+var movieArray = ["Spotlight", "Birdman", "12 Years a Slave", "Argo", "The Artist", "The Kings Speach", "The Hurt Locker", "Slumdog Millionaire", "No Country for Old Men", "The Departed", "Crash", "Million Dollar Baby", "The Return of the King", "Chicago", "A Beautiful Mind", "Gladiator", "American Beauty", "Titanic", "Shakespeare in Love", "The English Patient", "Braveheart", "Forest Gump", "Schindlers List", "Unforgiven", "Dances With Wolves", "Driving Miss Daisy", "The Last Emperor", "Platoon", "Out of Aferica", "Amadeus", "Terms Of Endearment", "Ganghi", "Chariots Of Fire", "Ordinary People", "Kramer vs Kramer", "The Deer Hunter", "Annie Hall", "Rocky", "One Flew Over The Cuckoos Nest", "The Godfather Part 2", "The Sting", "The Godfather", "The French Connection", "Patton","Midnight Cowboy", "Oliver"];
+var pressedButtonArray = [" "];
+var computerPick = "American Sniper";
+
+
+
+/* pick random movie from movieArray */
+function shoot() {
+    computerPick = movieArray[Math.floor(Math.random() * movieArray.length)];
+    console.log(computerPick);
+ 	};   
+
+
+$(document).ready(trackGuesses(){
+    $("#letters").keypress(function(){
+        $("span").text(guessCounter -= 1);
+    });
