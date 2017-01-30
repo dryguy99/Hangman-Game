1f2b2866e4d5b3a2eb91a46ae5cc9f3eb65d5ff8
Timothy Dry <timothy.dry@verizon.net>
2017-01-30 03:29:24 -0500

    started on Javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+/* define varibles*/
+var guessCounter = 12;
+var winCounter = 0;
+var lossCounter = 0;
+var movieArray = ["Spotlight", "Birdman", "12 Years a Slave", "Argo", "The Artist", "The Kings Speach", "The Hurt Locker", "Slumdog Millionaire", "No Country for Old Men", "The Departed", "Crash", "Million Dollar Baby", "The Return of the King", "Chicago", "A Beautiful Mind", "Gladiator", "American Beauty", "Titanic", "Shakespeare in Love", "The English Patient", "Braveheart", "Forest Gump", "Schindlers List", "Unforgiven", "Dances With Wolves", "Driving Miss Daisy", "The Last Emperor", "Platoon", "Out of Aferica", "Amadeus", "Terms Of Endearment", "Ganghi", "Chariots Of Fire", "Ordinary People", "Kramer vs Kramer", "The Deer Hunter", "Annie Hall", "Rocky", "One Flew Over The Cuckoos Nest", "The Godfather Part 2", "The Sting", "The Godfather", "The French Connection", "Patton","Midnight Cowboy", "Oliver"]
+var pressedButtonsArray = [" "]
