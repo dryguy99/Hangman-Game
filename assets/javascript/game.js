31d02936e29c5365fea3d9ff0560ed3135104e7c
Timothy Dry <timothy.dry@verizon.net>
2017-01-30 20:11:11 -0500

    got letter to appear on screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var winCounter = 0;
 var lossCounter = 0;
 var movieArray = ["Spotlight", "Birdman", "12 Years a Slave", "Argo", "The Artist", "The Kings Speach", "The Hurt Locker", "Slumdog Millionaire", "No Country for Old Men", "The Departed", "Crash", "Million Dollar Baby", "The Return of the King", "Chicago", "A Beautiful Mind", "Gladiator", "American Beauty", "Titanic", "Shakespeare in Love", "The English Patient", "Braveheart", "Forest Gump", "Schindlers List", "Unforgiven", "Dances With Wolves", "Driving Miss Daisy", "The Last Emperor", "Platoon", "Out of Aferica", "Amadeus", "Terms Of Endearment", "Ganghi", "Chariots Of Fire", "Ordinary People", "Kramer vs Kramer", "The Deer Hunter", "Annie Hall", "Rocky", "One Flew Over The Cuckoos Nest", "The Godfather Part 2", "The Sting", "The Godfather", "The French Connection", "Patton","Midnight Cowboy", "Oliver"];
 var pressedButton = "";
-var pressedButtonArray = [""];
+var pressedButtonArray = [" "];
 var computerPick = "American Sniper";
 var movietitleWithBlanks = " ";
 
@@ -25,7 +25,7 @@ function initialize() {
 initialize();
 
 function shoot() {
-	document.getElementById("letters").addEventListener("keyup", myFunction());
+	document.getElementById("letters").innerHTML = pressedButtonArray;
 }
 function myFunction() {
     var x = document.getElementById("letters");
@@ -35,6 +35,8 @@ function myFunction() {
 document.onkeyup = function(event) {
 	pressedButton = event.key;
 	alert(pressedButton);
+	pressedButtonArray.push(pressedButton + ", ");
+	shoot();
 } ;
 /* get keypress 
 $("#letters").keydown (function (e) {
