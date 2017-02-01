fc56b7f3cefa950ed745f9030c04dba624ebf2b2
Timothy Dry <timothy.dry@verizon.net>
2017-02-01 01:24:54 -0500

    mostly working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,37 +7,42 @@ var lossCounter = 0;
 var movieArray = ["Spotlight", "Birdman", "12 Years a Slave", "Argo", "The Artist", "The Kings Speach", "The Hurt Locker", "Slumdog Millionaire", "No Country for Old Men", "The Departed", "Crash", "Million Dollar Baby", "The Return of the King", "Chicago", "A Beautiful Mind", "Gladiator", "American Beauty", "Titanic", "Shakespeare in Love", "The English Patient", "Braveheart", "Forest Gump", "Schindlers List", "Unforgiven", "Dances With Wolves", "Driving Miss Daisy", "The Last Emperor", "Platoon", "Out of Aferica", "Amadeus", "Terms Of Endearment", "Ganghi", "Chariots Of Fire", "Ordinary People", "Kramer vs Kramer", "The Deer Hunter", "Annie Hall", "Rocky", "One Flew Over The Cuckoos Nest", "The Godfather Part 2", "The Sting", "The Godfather", "The French Connection", "Patton","Midnight Cowboy", "Oliver"];
 var pressedButton = "";
 var pressedButtonArray = [""];
-var computerPick = "American Sniper";
+var computerPick = "AMERICAN SNIPER";
+var computerPickU = "American Sniper";
 var movietitleWithBlanks = " ";
-var guessList = " ";
+var guessList = "";
 var inArray = 0;
 var checkArray = [];
 var indexN = 0;
 var movieTitleArray = [];
+var guessCorrect = "";
 
 
-/* pick random movie from movieArray 
-function shoot() {
-    
-    document.getElementById("movie").innerHTML = movietitleWithBlanks;
- 	};   */
+
+
 /* start game by picking  movie title at random and set spaces for words */
 function initialize() {
-	computerPick = movieArray[Math.floor(Math.random() * movieArray.length)];
+	pressedButtonArray = [];
+	guessCounter = 12;
+	guessList = "";
+	computerPickU = movieArray[Math.floor(Math.random() * movieArray.length)];
+	computerPick = computerPickU.toUpperCase();
     console.log(computerPick);
-    movietitleWithBlanks = computerPick.replace(/\w/g,"_ ");
+    movietitleWithBlanks = computerPick.replace(/ /g,"9").replace(/\D/g, "_").replace(/9/g, '-');
     document.getElementById("movie").innerHTML = movietitleWithBlanks;
     checkArray = computerPick.split("");
     movieTitleArray = movietitleWithBlanks.split("");
+    document.getElementById("guesscount").innerHTML = guessCounter;
+    document.getElementById("letters").innerHTML = guessList;
+
 }
 initialize();
 
 /* print guessed letters to screen*/
 function printLetters(){
   	guessList = "";
-  		for(var i=1; i < pressedButtonArray.length; ++i){
+  		for(var i=0; i < pressedButtonArray.length; ++i){
       		guessList = (guessList + pressedButtonArray[i] + " ");
-      		console.log("inside: " + guessList)
  			};
   	console.log("outside: " + guessList);
   	document.getElementById("letters").innerHTML = guessList;
@@ -54,6 +59,17 @@ function compareLetters(item) {
 		pressedButtonArray.push(item);
 		};	
 }
+
+function correctGuess() {
+		guessCorrect = "";
+  			for(var i=0; i < movieTitleArray.length; ++i){
+      			guessCorrect = (guessCorrect + movieTitleArray[i] + " ");
+ 			};
+  		console.log("outside: " + guessCorrect);
+  		document.getElementById("movie").innerHTML = guessCorrect;
+}
+ 		
+
 /* check to see if selected letter is in the title and display the letter in the correct place.*/
 function findLetters(item) {
 
@@ -62,23 +78,43 @@ function findLetters(item) {
  		var a = checkArray.indexOf(item);
  		movieTitleArray[a] = item;
  		a += 1;
+ 		correctGuess();
  		console.log("index: " + a + " " + item + " " + movieTitleArray);
  		while ((checkArray.length - 1) >= a) {
  			if ((checkArray.indexOf(item, a) > 0) && (checkArray.indexOf(item, a) < checkArray.length)) {
  				var b = checkArray.indexOf(item, a);
  				movieTitleArray[b] = item;
  				a = b + 1;
- 				console.log("WL - index: " + a + " " + item + " " + movieTitleArray);
+ 				correctGuess();
+ 				console.log("WL - index: " + a + b + " " + item + " " + movieTitleArray);
  			}
  				else {a += 1;}
  		}
+ 		a = 0;
+ 	}
+ 	else {
+ 		guessCounter = guessCounter - 1;
+ 		document.getElementById("guesscount").innerHTML = guessCounter;
+ 	}
+ 	
+ }
+ function winorLose() {
+ 	if (movieTitleArray.indexOf("_") < 0) {
+ 		// you won message - update picture - update win count -
+ 		document.getElementById("answer").innerHTML = computerPickU;
+ 		if (confirm("YOU WON!\nPlay Again?")) {
+ 			initialize();
+ 		}
+ 	}
+ 	else if (guessCounter === 0) {
+ 		// you lost update picture - update loss count
+ 		
  	}
  }
- 
 
 document.onkeyup = function(event) {
-	pressedButton = event.key;
-	alert(pressedButton);
+	var pressedButtonL = event.key;
+	pressedButton = pressedButtonL.toUpperCase();
 	
 	if (inArray === 0) {
 		pressedButtonArray.push(pressedButton);
@@ -87,6 +123,7 @@ document.onkeyup = function(event) {
 	findLetters(pressedButton);
 	compareLetters(pressedButton);
 	printLetters();
+	winorLose();
 } 
 /* get keypress 
 $("#letters").keydown (function (e) {
