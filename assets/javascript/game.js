f299d7641852e78290352e83e02edd1b3506c046
Timothy Dry <timothy.dry@verizon.net>
2017-02-01 14:07:51 -0500

    picture switch is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 /* add jQuery to javascript*/
 
 /* define varibles*/
-var guessCounter = 12;
+var guessCounter = 10;
 var winCounter = 0;
 var lossCounter = 0;
-var movieArray = ["Spotlight", "Birdman", "12 Years a Slave", "Argo", "The Artist", "The Kings Speach", "The Hurt Locker", "Slumdog Millionaire", "No Country for Old Men", "The Departed", "Crash", "Million Dollar Baby", "The Return of the King", "Chicago", "A Beautiful Mind", "Gladiator", "American Beauty", "Titanic", "Shakespeare in Love", "The English Patient", "Braveheart", "Forest Gump", "Schindlers List", "Unforgiven", "Dances With Wolves", "Driving Miss Daisy", "The Last Emperor", "Platoon", "Out of Aferica", "Amadeus", "Terms Of Endearment", "Ganghi", "Chariots Of Fire", "Ordinary People", "Kramer vs Kramer", "The Deer Hunter", "Annie Hall", "Rocky", "One Flew Over The Cuckoos Nest", "The Godfather Part 2", "The Sting", "The Godfather", "The French Connection", "Patton","Midnight Cowboy", "Oliver", "In The Heat Of THe Night", "A Man For All Seasons", "The Sound Of Music"];
-var moviepicArray = ["http://www.filmsite.org/moviescreens/spotlight.jpg", "http://www.filmsite.org/pictures/birdman.jpg", "http://www.filmsite.org/credits/12yearsaslave.jpg", "http://www.filmsite.org/screens/argo.jpg", "http://www.filmsite.org/posters/artist-titlescreen.jpg", "http://www.filmsite.org/posters/kingsspeechtitle.jpg", "http://www.filmsite.org/posters/hurtlockertitle.jpg", "http://www.filmsite.org/titles/slumdogmill.jpg", "http://www.filmsite.org/filmfotos/nocountryforoldmen_title.jpg", "http://www.filmsite.org/filmfotos/departed.jpg", "http://www.filmsite.org/titles/crash2005.jpg", "http://www.filmsite.org/titles/milliondollarbaby-title.jpg", "http://www.filmsite.org/titles/lordofringsretofkingp2.jpg", "http://www.filmsite.org/titles/chicago.jpg", "http://www.filmsite.org/titles/beautifulmind-title.jpg", "http://www.filmsite.org/titles/gladiator-title.jpg", "http://www.filmsite.org/titles/americanbeauty-title.jpg", "http://www.filmsite.org/titles/shakespeareinlove-title.jpg", "http://www.filmsite.org/titles/titanic-title.jpg", "http://www.filmsite.org/titles/englishpatient-title.jpg", "http://www.filmsite.org/titles/braveheart-title.jpg", "http://www.filmsite.org/titles/forrestgump-title.jpg", "http://www.filmsite.org/titles/schindlerslist-title.jpg", "http://www.filmsite.org/titles/unforgiven-title.jpg", "http://www.filmsite.org/titles/silenceoflambs-title.jpg", "http://www.filmsite.org/titles/danceswithwolves-title.jpg", "http://www.filmsite.org/screens/drivingmissdaisy.jpg", "http://www.filmsite.org/titles/rainman-title.jpg", "http://www.filmsite.org/titles/lastemperor.jpg", "http://www.filmsite.org/titles/platoon-title.jpg", "http://www.filmsite.org/screens/outofafrica.jpg", "http://www.filmsite.org/titles/amadeus-title.jpg", "http://www.filmsite.org/titles/termsofendearment-title.jpg", "http://www.filmsite.org/titles/gandhi-title.jpg", "http://www.filmsite.org/filmfotos/chariotsfire.jpg", "http://www.filmsite.org/filmfotos/ordinarypeople6.jpg", "http://www.filmsite.org/filmfotos/kramer5.jpg", "http://www.filmsite.org/titles/deerhunter.jpg", "http://www.filmsite.org/titles/anniehall.jpg", "http://www.filmsite.org/titles/rocky-title.jpg", "http://www.filmsite.org/titles/oneflewovercuckoosnest-titl.jpg", "http://www.filmsite.org/titles/godfather1974-title.jpg", "http://www.filmsite.org/titles/sting-title.jpg", "http://www.filmsite.org/titles/godfather1972-title.jpg", "http://www.filmsite.org/titles/frenchconnection-title.jpg", "http://www.filmsite.org/titles/patton-title.jpg", "http://www.filmsite.org/titles/midnightcowboy-title.jpg", "http://www.filmsite.org/titles/oliver.jpg", "http://www.filmsite.org/titles/intheheatofnight-title.jpg", "http://www.filmsite.org/titles/manforallseasons.jpg", "http://www.filmsite.org/titles/soundofmusic-title.jpg"]
+var movieArray = ["Spotlight", "Birdman", "12 Years a Slave", "Argo", "The Artist", "The Kings Speach", "The Hurt Locker", "Slumdog Millionaire", "No Country for Old Men", "The Departed", "Crash", "Million Dollar Baby", "The Return of the King", "Chicago", "A Beautiful Mind", "Gladiator", "American Beauty", "Titanic", "Shakespeare in Love", "The English Patient", "Braveheart", "Forest Gump", "Schindlers List", "Unforgiven", "Silence Of The Lambs", "Dances With Wolves", "Driving Miss Daisy", "Rainman", "The Last Emperor", "Platoon", "Out of Africa", "Amadeus", "Terms Of Endearment", "Ganghi", "Chariots Of Fire", "Ordinary People", "Kramer vs Kramer", "The Deer Hunter", "Annie Hall", "Rocky", "One Flew Over The Cuckoos Nest", "The Godfather Part 2", "The Sting", "The Godfather", "The French Connection", "Patton","Midnight Cowboy", "Oliver", "In The Heat Of THe Night", "A Man For All Seasons", "The Sound Of Music"];
+var moviepicArray = ["http://www.filmsite.org/moviescreens/spotlight.jpg", "http://www.filmsite.org/pictures/birdman.jpg", "http://www.filmsite.org/credits/12yearsaslave.jpg", "http://www.filmsite.org/screens/argo.jpg", "http://www.filmsite.org/posters/artist-titlescreen.jpg", "http://www.filmsite.org/posters/kingsspeechtitle.jpg", "http://www.filmsite.org/posters/hurtlockertitle.jpg", "http://www.filmsite.org/titles/slumdogmill.jpg", "http://www.filmsite.org/filmfotos/nocountryforoldmen_title.jpg", "http://www.filmsite.org/filmfotos/departed.jpg", "http://www.filmsite.org/titles/crash2005.jpg", "http://www.filmsite.org/titles/milliondollarbaby-title.jpg", "http://www.filmsite.org/titles/lordofringsretofkingp2.jpg", "http://www.filmsite.org/titles/chicago.jpg", "http://www.filmsite.org/titles/beautifulmind-title.jpg", "http://www.filmsite.org/titles/gladiator-title.jpg", "http://www.filmsite.org/titles/americanbeauty-title.jpg", "http://www.filmsite.org/titles/titanic-title.jpg", "http://www.filmsite.org/titles/shakespeareinlove-title.jpg", "http://www.filmsite.org/titles/englishpatient-title.jpg", "http://www.filmsite.org/titles/braveheart-title.jpg", "http://www.filmsite.org/titles/forrestgump-title.jpg", "http://www.filmsite.org/titles/schindlerslist-title.jpg", "http://www.filmsite.org/titles/unforgiven-title.jpg", "http://www.filmsite.org/titles/silenceoflambs-title.jpg", "http://www.filmsite.org/titles/danceswithwolves-title.jpg", "http://www.filmsite.org/screens/drivingmissdaisy.jpg", "http://www.filmsite.org/titles/rainman-title.jpg", "http://www.filmsite.org/titles/lastemperor.jpg", "http://www.filmsite.org/titles/platoon-title.jpg", "http://www.filmsite.org/screens/outofafrica.jpg", "http://www.filmsite.org/titles/amadeus-title.jpg", "http://www.filmsite.org/titles/termsofendearment-title.jpg", "http://www.filmsite.org/titles/gandhi-title.jpg", "http://www.filmsite.org/filmfotos/chariotsfire.jpg", "http://www.filmsite.org/filmfotos/ordinarypeople6.jpg", "http://www.filmsite.org/filmfotos/kramer5.jpg", "http://www.filmsite.org/titles/deerhunter.jpg", "http://www.filmsite.org/titles/anniehall.jpg", "http://www.filmsite.org/titles/rocky-title.jpg", "http://www.filmsite.org/titles/oneflewovercuckoosnest-titl.jpg", "http://www.filmsite.org/titles/godfather1974-title.jpg", "http://www.filmsite.org/titles/sting-title.jpg", "http://www.filmsite.org/titles/godfather1972-title.jpg", "http://www.filmsite.org/titles/frenchconnection-title.jpg", "http://www.filmsite.org/titles/patton-title.jpg", "http://www.filmsite.org/titles/midnightcowboy-title.jpg", "http://www.filmsite.org/titles/oliver.jpg", "http://www.filmsite.org/titles/intheheatofnight-title.jpg", "http://www.filmsite.org/titles/manforallseasons.jpg", "http://www.filmsite.org/titles/soundofmusic-title.jpg"]
 var pressedButton = "";
 var pressedButtonArray = [""];
 var computerPick = "AMERICAN SNIPER";
@@ -17,6 +17,10 @@ var checkArray = [];
 var indexN = 0;
 var movieTitleArray = [];
 var guessCorrect = "";
+//for (i=0; i < movieArray.length; ++i) {
+//	console.log(i + "=" + movieArray[i] + " - " + moviepicArray[i])
+//}
+
 
 
 
@@ -24,7 +28,7 @@ var guessCorrect = "";
 /* start game by picking  movie title at random and set spaces for words */
 function initialize() {
 	pressedButtonArray = [];
-	guessCounter = 12;
+	guessCounter = 10;
 	guessList = "";
 	computerPickU = movieArray[Math.floor(Math.random() * movieArray.length)]; /*pick random movie*/
 	computerPick = computerPickU.toUpperCase(); /*convert to all uppercase*/
@@ -94,7 +98,7 @@ function findLetters(item) {
  		a = 0;
  	}
  	else {
- 		guessCounter = guessCounter - 1;
+ 		guessCounter--;
  		document.getElementById("guesscount").innerHTML = guessCounter;
  	}
  	
@@ -104,9 +108,11 @@ function findLetters(item) {
  	if (movieTitleArray.indexOf("_") < 0) {
  		// you won message - update picture - update win count -
  		winCounter++;
+ 		var b = movieArray.indexOf(computerPickU)
+ 		document.getElementById("moviepic").src = moviepicArray[b];
  		console.log("after increment: " + winCounter);
- 		document.getElementById("wincount").innerHTML = (winCounter + " - ");
- 		document.getElementById("losscount").innerHTML = lossCounter;
+ 		document.getElementById("wincount").innerHTML = (winCounter + " -");
+ 		document.getElementById("losscount").innerHTML = ("- " + lossCounter);
  		document.getElementById("answer").innerHTML = computerPickU;
 
  		if (confirm("YOU WON!\nPlay Again?")) {
